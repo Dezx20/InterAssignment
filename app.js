9f2fbb5035d661d1cf3e125bb1f43ff2f08597d2
Dezx20 <2009awektoppo@gmail.com>
2021-09-03 15:13:25 +0530

    stylesheet added seperately

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/', indexRouter);
 app.use('/api', usersRouter);
